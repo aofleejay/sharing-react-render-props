dc8991e2453b154e550eca3876116e15a14c2ad9
aofleejay <aofleejay@hotmail.com>
2018-05-26 01:23:57 +0700

    Rename variable

--- src/components/MusicStore.js
+++ src/components/MusicStore.js
@@ -4,10 +4,10 @@ import DataConnector from './DataConnector'
 const MusicStore = () => (
   <DataConnector
     url="https://jsonplaceholder.typicode.com/albums"
-    render={(list) => (
+    render={(albums) => (
       <div>
         <h1>Music Store</h1>
-        { list.length > 0 && list.map(({ id, title }) => <li key={id}>{title}</li>) }
+        { albums.length > 0 && albums.map(({ id, title }) => <li key={id}>{title}</li>) }
       </div>
     )}
   />
